6bdf3a80f4aa82217dfe1ca6b2b42dce2d66be7e
Vigneshwaran <vigneshwaran.surulivel@gmail.com>
2022-09-08 20:45:59 +0530

    initial commit

---
+++ countdown-timer/script.js
@@ -0,0 +1,33 @@
+const newYear = "1 jan 2023"
+const hoursEl = document.getElementById("hours")
+const daysEl = document.getElementById("days")
+const minutesEl = document.getElementById("minutes")
+const secondsEl = document.getElementById("seconds")
+
+
+function countdown(){
+    const newYearDate = new Date(newYear);
+    const currDate = new Date();
+    const numberOfSeconds = (newYearDate-currDate)/1000
+
+    const days = Math.floor(numberOfSeconds / 3600 / 24)
+    const hours = Math.floor(numberOfSeconds / 3600) % 24
+    const minutes = Math.floor(numberOfSeconds / 60) % 60
+    const seconds = Math.floor(numberOfSeconds % 60)
+
+    daysEl.innerHTML = formatTime(days)
+    hoursEl.innerHTML = formatTime(hours)
+    minutesEl.innerHTML = formatTime(minutes)
+    secondsEl.innerHTML = formatTime(seconds)
+
+    console.log(days, hours, minutes, seconds)
+
+}
+
+function formatTime(time){
+    return time<10 ? `0${time}` : time
+}
+
+countdown()
+
+setInterval(countdown, 1000)
