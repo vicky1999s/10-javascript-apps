df7cbe15afd4326a08ddb26c0110857891795d02
Vigneshwaran <vigneshwaran.surulivel@gmail.com>
2022-09-10 22:29:26 +0530

    notes-app added

---
+++ notes-app.js
@@ -0,0 +1,71 @@
+const notes = JSON.parse(localStorage.getItem("notes"))
+const notesContainter = document.getElementById("content-container")
+const addButton = document.getElementsByClassName("btn")
+
+if(notes){
+    notes.forEach((note)=>{
+        console.log(note)
+        addNewNote(note)
+    })
+}
+
+addButton[0].addEventListener("click", (e)=>{
+    addNewNote("")
+})
+
+function addNewNote(text){
+    const note = document.createElement("div")
+    note.classList.add("notes")
+    note.innerHTML=`
+            <div class="icons">
+                <button class="btn edit-icon">
+                    <i class="fa-solid fa-pen-to-square"></i>
+                </button>
+                <button class="btn trash-icon">
+                    <i class="fa-solid fa-trash"></i>
+                </button>
+            </div>
+            <div class="hr"></div>
+            <div class="textarea">
+                <textarea id="notes1" disabled="true"></textarea>
+            </div>
+            `
+    
+    let disabled = true
+    const editbtn = note.querySelector(".edit-icon")
+    const textarea = note.querySelector("#notes1")
+    const editicon = note.querySelector(".edit-icon")
+    const deletebtn = note.querySelector(".trash-icon")
+
+    textarea.value = text
+
+    editbtn.addEventListener("click", ()=>{
+        textarea.disabled = !disabled
+        editicon.classList.toggle("selected")
+
+    })
+
+    deletebtn.addEventListener("click", ()=>{
+        notesContainter.removeChild(note)
+    })
+
+    textarea.addEventListener("input", (e)=>{
+        const {value} = e.target
+        updateLS()
+    })
+
+    notesContainter.appendChild(note)
+
+}
+
+function updateLS() {
+    const notesText = document.querySelectorAll("#notes1");
+
+    const notes = [];
+
+    notesText.forEach((note) => {
+        note.value ? notes.push(note.value):false;
+    });
+
+    localStorage.setItem("notes", JSON.stringify(notes));
+}
